173c8a34af2f8937b4ef8290d4e11f5e84f3bfad
shareef <70265506+sherif414@users.noreply.github.com>
2022-08-12 16:44:49 +0200

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid min-h-screen gap-0 p-8">
+    <button
+      class="justify-self-center border rounded py-2 px-4 i-material-symbols-light-mode-outline dark:i-material-symbols-dark-mode-outline "
+      @click="toggleDark()" />
+
+    <h1 class="text-4rem text-center">
+      Hello Fucking World !
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+</style>
---
+++ unocss.config.ts
@@ -0,0 +1,30 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  // transformerDirectives,
+  // transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [],
+  presets: [
+    presetUno(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono',
+      },
+    }),
+  ],
+  // transformers: [
+  //   transformerDirectives(),
+  //   transformerVariantGroup(),
+  // ],
+})
